9b88dac3ad99e7ed96ad8628d907248f41beae02
A.Adefolalu B <adefolalucode101@gmail.com>
2022-07-30 22:24:35 +0100

    add jquery code to js file

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+// function sqrt(num){
+//     let formula = num * num;
+//     return formula;
+// }
+// sqrt;
+$(".squareR").click(function(){
+    $("#vision").val($("#vision").val().math.sqrt(num));
+    });
+function pow(num){
+    let cal = num**;
+    return cal;
+}
+pow;
+
+});
